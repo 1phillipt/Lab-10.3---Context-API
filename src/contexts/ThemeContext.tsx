bc892a0fe7735d34eec4bc713a6cee6d742917e8
Phillip Thapa <1thapat@gmail.com>
2026-03-20 11:49:00 -0400

    created custom hooks so themContext is accessed throughout application

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,12 +1,40 @@
+import { useState, createContext,type ReactNode, useContext } from "react";
 import type { ThemeType } from "../types";
 
 interface ThemeContextType{
     theme: ThemeType;
-    toggleThem: ()=>void;
+    toggleTheme: ()=>void;
 }
 
 //create the context
 
 const ThemeContext = createContext<ThemeContextType |undefined>(undefined);
+
+export function ThemeProvider({children}:{children:ReactNode}){
+
+    //state for theme
+    const [theme, setTheme] = useState<ThemeType>("light");
+
+    // change theme with this function
+    const toggleTheme = ()=>{setTheme(prev => (prev === "light"?"dark":"light"))};
+
+    return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+
+}
+
+//custom hook to use context
+export function useTheme(){
+    const context = useContext(ThemeContext);
+
+    if(!ThemeContext){
+        throw new Error("useThem must be used within THemeProvider")
+    }
+
+    return context;
+}
 
 
