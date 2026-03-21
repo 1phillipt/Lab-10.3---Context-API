5c0db36dcb30fc72720ee89011a5217d3245cde5
Phillip Thapa <1thapat@gmail.com>
2026-03-20 21:03:06 -0400

    working on compnent todoList edit logic

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTodo } from "../contexts/TodoContext";
 import { useFilter } from "../contexts/FilterContext";
 
@@ -7,8 +7,11 @@ import { useFilter } from "../contexts/FilterContext";
 export function TodoList(){
 
     //disconstructing the context todoContext
-    const {todos, toggleTodo, deleteTodo} = useTodo();
+    const {todos, toggleTodo, deleteTodo, editTodo} = useTodo();
     const {filter} = useFilter();
+
+    const [editingId, setEditingId]=useState<string | null>(null);
+    const [editText, setEditText] = useState("");
 
     //filter todos based on the current filter state (all, active, completed)
         const filteredTodos = todos.filter(todo => {
@@ -25,18 +28,36 @@ export function TodoList(){
             No todos yet!
         </div>
     }
+  
+
     return (
         <ul>
-            {
-                filteredTodos.map((todo) => (
-                    <li key={todo.id} onClick={()=>toggleTodo(todo.id)}>
-                       <span>
+            {filteredTodos.map(todo => (
+                <li key={todo.id}>
+                    {editingId === todo.id ? (
+                    <>
+                    <input value={editText} onChange={e =>setEditText(e.target.value)}/>
+                    <button onClick={()=> {
+                        editTodo(todo.id, editText)
+                        setEditingId(null);
+                        setEditText("");
+                    }}>Save</button>
+                    <button>cancel</button>
+                  
+                    </>): (<>
+                    <span>
                         {todo.text}
-                        </span> 
-                        <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
-                    </li>
-                ))
-            }
+                    </span>
+                    <button onClick={()=>{setEditingId(todo.id); setEditText(todo.text)}}>
+                        Edit
+                    </button>
+                        
+                    
+                    </>)
+}
+                </li>
+
+            ))}
         </ul>
     )
 }
